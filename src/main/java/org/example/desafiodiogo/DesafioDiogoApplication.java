dc65d388303c782a524acd8005a4233bb2432926
Pedro Gabriel <pedro.correia@germinare.org.br>
2026-01-30 08:45:13 -0300

    Inital Commit

---
+++ src/main/java/org/example/desafiodiogo/DesafioDiogoApplication.java
@@ -0,0 +1,13 @@
+package org.example.desafiodiogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDiogoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioDiogoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/desafiodiogo/DesafioDiogoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.desafiodiogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDiogoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
