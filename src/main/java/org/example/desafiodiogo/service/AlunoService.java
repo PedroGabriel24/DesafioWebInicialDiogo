ede001f9a9ef664d8f1865c7551e57c7b7bef1c7
Pedro Gabriel <pedro.correia@germinare.org.br>
2026-02-22 16:24:16 -0300

    feat: genereta boletim pdf

--- src/main/java/org/example/desafiodiogo/service/AlunoService.java
+++ src/main/java/org/example/desafiodiogo/service/AlunoService.java
@@ -4,6 +4,7 @@ import org.example.desafiodiogo.dto.aluno.AlunoSerieRequest;
 import org.example.desafiodiogo.dto.aluno.AlunoSerieResponse;
 import org.example.desafiodiogo.dto.aluno.BoletimResponse;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -12,6 +13,8 @@ public interface AlunoService {
     AlunoSerieResponse adicionarAlunoASerie(AlunoSerieRequest request);
 
     BoletimResponse obterBoletimAluno();
+
+    byte[] gerarBoletimPdf() throws IOException;
 
     List<Map<String, Object>> listarMaterias();
 
